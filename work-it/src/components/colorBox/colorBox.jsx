202a2a86efe68c32bcfda1863b93c5167b4042d1
hae-on <solbi2004@naver.com>
2021-10-25 19:24:01 +0900

    [add]box 줄바꿈

--- work-it/src/components/colorBox/colorBox.jsx
+++ work-it/src/components/colorBox/colorBox.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-// import { ColorConsumer } from "./colorContext";
 import styles from "./colorBox.module.css";
 import Box from "components/box/box";
 
 const ColorBox = ({ boxes }) => {
   return (
     <section className={styles.colorbox}>
-      {boxes && boxes.map((box) => <Box box={box} />)}
+      {boxes && boxes.map((box) => <Box box={box} key={box.id} />)}
     </section>
   );
 };
